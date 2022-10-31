17bd1d3a86a1fe2694b92ffa01b5babcd7dd4757
Vutomi Luigi Khosa <khosavutomi99@gmail.com>
2022-10-31 12:28:07 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h3 className="nav--logo_text">Charlie Nghondzweni</h3>
+            <h4 className="nav--title">Portfolio</h4>
+        </nav>
+    )
+}
